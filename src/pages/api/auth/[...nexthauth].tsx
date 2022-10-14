5919b7565d5dc7f9610812399fd9d17375a8af2b
Gayuh kautaman <gkoma444@gmail.com>
2022-10-14 13:57:31 +0700

    added auth feature

--- src/pages/api/auth/[...nexthauth].tsx
+++ src/pages/api/auth/[...nexthauth].tsx
@@ -12,7 +12,40 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   callbacks: {},
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        const user = await prisma.user.findUnique({
+          where: {
+            userEmail: credentials.username,
+          },
+        });
+        if (
+          user &&
+          (await argon2.verify(user.password, credentials.password))
+        ) {
+          return {
+            _id: user.id,
+            name: user.fullname,
+            email: user.userEmail,
+            image: "f",
+            isAdmin: user.isAdmin,
+          };
+        }
+        throw new Error("Invalid email or password");
+      },
+      credentials: {
+        domain: {
+          label: "Domain",
+          type: "text ",
+          placeholder: "CORPNET",
+          value: "CORPNET",
+        },
+        username: { label: "Username", type: "text ", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+    }),
+  ],
   secret: process.env.NEXTAUTH_SECRET,
   // useSecureCookies: true,
 };
