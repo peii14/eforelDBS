f118f891bb83216e1fcb4b8a0ade7820f8666e05
satria-ui <dimbum14@gmail.com>
2022-11-17 00:43:16 +0700

    update db, fix seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+import moment, { Moment } from "moment";
 
-
+const prisma = new PrismaClient();
 
 async function main(){
     for(let i = 0; i < 100; i++){
@@ -9,12 +11,21 @@ async function main(){
                 customer_name:`${faker.name.fullName()}`,
                 customer_code:`dsfkl`,
                 customer_salesCode:`${faker.address.countryCode('alpha-2')}`,
-                vertical_market:'industry',
                 customer_address:`${faker.address.streetAddress()}`,
                 customer_province:`${faker.address.country()}`,
                 customer_city:`${faker.address.city()}`,
-                customer_postal_code:`${faker.address.zipCode()}`,
+                customer_postalCode:`${faker.address.zipCode()}`,
                 customer_phone:`${faker.phone.number('+62 ### ## ##')}`,
+
+                PIC:{
+                    create:{
+                        pic_name:`${faker.name.fullName()}`,
+                        pic_position:`Sales`,
+                        pic_email:`${faker.internet.email()}`,
+                        pic_phone:`${faker.phone.number('+62 ### ## ##')}`,
+                        pic_sales_code:`${faker.address.countryCode('alpha-2')}`,
+                    }
+                },
 
                 Quotation:{
                     create:{
@@ -31,38 +42,79 @@ async function main(){
                         },
                         SalesPipeline:{
                             create:{
-                                salesPipeline_date:`${faker.date.recent()}`,
-                                salesPipeline_closeEstimation:`${faker.date.recent()}`,
+                                salesPipeline_date:new Date().toISOString().slice(0, 19).replace('T', ' '),
+                                salesPipeline_closeEstimation:new Date().toISOString().slice(0, 19).replace('T', ' '),
                                 salesPipeline_salesCode:`${faker.address.countryCode('alpha-2')}`,
-                                salesPipeline_cust_name:`${faker.name.fullName()}`,
-                                project_name:`${faker.name.jobTitle()}`,
-                                product:`${faker.commerce.product()}`,
-                                quantity:Number(faker.random.numeric(1)),
-                                Pi_status:`${faker.random.alphaNumeric(5)}`,
-                                M_num:`${faker.random.alphaNumeric(7)}`,
-                                close_date:`${faker.date.recent()}`,
-                                note:`${faker.commerce.productDescription()}`
+                                salesPipeline_projectName:`${faker.name.jobTitle()}`,
+                                salesPipeline_product:`${faker.commerce.product()}`,
+                                salesPipeline_quantity:Number(faker.random.numeric(1)),
+                                salesPipeline_status:`${faker.random.alphaNumeric(5)}`,
+                                salesPipeline_closeDate:new Date().toISOString().slice(0, 19).replace('T', ' '),
+                                salesPipeline_note:`${faker.commerce.productDescription()}`
                             }
                         }
                     }
                 },
+
                 VerticalMarket:{
                     create:{
-                        vertical_market:'industry',
+                        verticalMarket_name:`${faker.company.companySuffix()}`,
+
                         Group:{
                             create:{
-                                group:`${faker.company.name()}`
+                                group_name:`${faker.company.companySuffix()}`
                             }
                         }
                     }
-                },
-                salesActivity:{
-                    create:{
-                        date:`${faker.date.recent()}`,
-                        type_followup:faker.datatype.boolean(),
-                        job_desc:`${faker.company.catchPhrase()}`
-                    }
-                },
+                }
+
+                // Quotation:{
+                //     create:{
+                //         quotation_num:`${faker.random.alphaNumeric(7)}`,
+                //         quotation_value:`${faker.commerce.product()}`,
+                //         quotation_product:`${faker.commerce.productName()}`,
+                //         quotation_quantity:Number(faker.random.numeric(1)),
+
+                //         MOP:{
+                //             create:{
+                //                 mop_num:`${faker.random.alphaNumeric(7)}`,
+                //                 mop_value:`${faker.commerce.product()}`
+                //             }
+                //         },
+                //         SalesPipeline:{
+                //             create:{
+                //                 salesPipeline_date:`${faker.date.recent()}`,
+                //                 salesPipeline_closeEstimation:`${faker.date.recent()}`,
+                //                 salesPipeline_salesCode:`${faker.address.countryCode('alpha-2')}`,
+                //                 salesPipeline_cust_name:`${faker.name.fullName()}`,
+                //                 project_name:`${faker.name.jobTitle()}`,
+                //                 product:`${faker.commerce.product()}`,
+                //                 quantity:Number(faker.random.numeric(1)),
+                //                 Pi_status:`${faker.random.alphaNumeric(5)}`,
+                //                 M_num:`${faker.random.alphaNumeric(7)}`,
+                //                 close_date:`${faker.date.recent()}`,
+                //                 note:`${faker.commerce.productDescription()}`
+                //             }
+                //         }
+                //     }
+                // },
+                // VerticalMarket:{
+                //     create:{
+                //         vertical_market:'industry',
+                //         Group:{
+                //             create:{
+                //                 group:`${faker.company.name()}`
+                //             }
+                //         }
+                //     }
+                // },
+                // salesActivity:{
+                //     create:{
+                //         date:`${faker.date.recent()}`,
+                //         type_followup:faker.datatype.boolean(),
+                //         job_desc:`${faker.company.catchPhrase()}`
+                //     }
+                // },
                 // PIC:{
                 //     create:{
                 //         P_name:"testing",
@@ -76,6 +128,13 @@ async function main(){
         })
     }
 }
+// async function main() {
+//     await prisma.customer.create({
+//         data:{
+//             customer_name
+//         }
+//     })
+// }
 
 main().catch(e => {
     console.log(e);
